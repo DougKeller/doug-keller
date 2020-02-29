7fa9ac3ad387d1bf9ad765e511ad4732174bb1d7
Doug Keller <dkeller4113@gmail.com>
2020-02-28 22:19:14 -0500

    Dynamically assign/display resume info

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,6 +12,8 @@ export class HomeComponent implements OnInit {
   ];
   textsToShow: number = 0;
 
+  data: Array<any>;
+
   ngOnInit(): void {
     let interval = setInterval(() => {
       this.textsToShow += 1;
@@ -19,6 +21,65 @@ export class HomeComponent implements OnInit {
         clearInterval(interval);
       }
     }, 750);
+
+    this.data = [
+      {
+        category: 'About Me',
+        paragraphs: [
+          'I am a full-stack software engineer and team lead with specializations in Ruby on Rails and AngularJS. I have experience with continuous integration deployment to AWS via Jenkins and containerization via Docker.',
+          'I am currently working on Big Biller, an applicant-tracking software for independent recruiting firms that uses Solr for full-text searching resumes to help recruiters make job placements.'
+        ]
+      },
+      {
+        category: 'Experience',
+        items: [
+          {
+            titleLeft: 'Top Echelon Software, LLC',
+            titleRight: '2019 - Present',
+            subtitle: 'Software Engineer, Team Lead'
+            // description: 'Description goes here'
+          },
+          {
+            titleLeft: 'Patriot Software',
+            titleRight: '2017 - 2019',
+            subtitle: 'Software Engineer, Team Lead'
+            // description: 'Description goes here'
+          },
+          {
+            titleLeft: 'UA Department of Electrical Engineering',
+            titleRight: '2017',
+            subtitle: 'Software Engineer Intern'
+          },
+          {
+            titleLeft: 'Patriot Software',
+            titleRight: '2015 - 2017',
+            subtitle: 'Software Engineer Intern'
+            // description: 'Description goes here'
+          }
+        ]
+      },
+      {
+        category: 'Education',
+        items: [
+          {
+            titleLeft: 'The University of Akron',
+            titleRight: 'Graduated 2017',
+            subtitle: 'BSCS - Computer Science',
+            // description: 'Description goes here'
+          }
+        ]
+      },
+      {
+        category: 'Skills',
+        list: [
+          'AngularJS',
+          'Ruby on Rails',
+          'Amazon Web Services',
+          'Docker Compose',
+          'Jenkins'
+        ]
+      }
+    ];
   }
 
   scrollToElement(element: HTMLElement): void {
